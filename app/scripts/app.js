8f77558c298717943176fc7e6b8787bf6afd4818
Richard Kalehoff <richardkalehoff@gmail.com>
2016-01-24 19:22:01 -0800

    add state management config

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,4 +9,33 @@
  * Main module of the application.
  */
 angular
-  .module('routingQuizApp', ['ui.router']);
+  .module('routingQuizApp', ['ui.router'])
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'views/instructions.html'
+      })
+      .state('redBrick', {
+        url: '/bricks/red',
+        templateUrl: 'views/bricks.html',
+        controller: 'RedBricksCtrl as brick'
+      })
+      .state('blueBrick', {
+        url: '/bricks/blue',
+        templateUrl: 'views/bricks.html',
+        controller: 'BlueBricksCtrl as brick'
+      })
+      .state('greenBrick', {
+        url: '/bricks/green',
+        templateUrl: 'views/bricks.html',
+        controller: 'GreenBricksCtrl as brick'
+      })
+      .state('cart', {
+        url: '/cart',
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl as cart'
+      });
+  }]);
